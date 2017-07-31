54cfa7056095155c8242330576d7aaf85fa0f855
Dan Selzer <dselzer@optonline.net>
2017-07-31 17:54:52 -0400

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
   if (!ObjectID.isValid(id)) {
-    res.status(404).send();
+    return res.status(404).send();
   }
   else {
     Todo.findById(id).then((todo) => {
@@ -50,6 +50,23 @@ app.get('/todos/:id', (req, res) => {
     });
   }
 
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (todo) {
+      res.send({todo});
+    }
+    else {
+      res.status(404).send();
+    }
+  }, (e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
