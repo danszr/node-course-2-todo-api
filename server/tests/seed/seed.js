683a8a1d25e632b7403856fb0a7f954111fef909
Dan Selzer <dselzer@optonline.net>
2017-08-02 16:15:40 -0400

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -3,18 +3,22 @@ const {Todo} = require('./../../models/todo');
 const {User} = require('./../../models/user');
 const jwt = require('jsonwebtoken');
 
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
-const userOneId = new ObjectID();
-const userTwoId = new ObjectID();
+
 
 const users = [{
   _id: userOneId,
@@ -27,7 +31,11 @@ const users = [{
 }, {
   _id: userTwoId,
   email:'jen@zhang.com',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+  }]
 }];
 
 const populateTodos = (done) => {
